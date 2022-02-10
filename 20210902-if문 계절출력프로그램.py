16d979e08b8ec04e34d51542c65700fa10b6e825
lgw0328 <99380730+lgw0328@users.noreply.github.com>
2022-02-10 13:40:17 +0900

    Add files via upload

---
+++ 20210902-if문 계절출력프로그램.py
@@ -0,0 +1,11 @@
+#if문 계절출력프로그램
+a = int(input())
+
+if 11 == a or 12 == a or 1 == a :
+    print("winter")
+if 2 == a or 3 == a or 4 == a :
+    print("spring")
+if 5 == a or 6 == a or 7 == a :
+    print("summer")
+if 8 == a or 9 == a or 10 == a :
+     print("fall")
